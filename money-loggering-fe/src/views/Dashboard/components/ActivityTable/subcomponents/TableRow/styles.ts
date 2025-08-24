3633f50fc30758e9c3647a8c75d3d90b2584ddfd
Franz Dizon <frdizon@live.com>
2025-08-24 12:45:06 +0800

    [FE] Remove old ActivityTable subcomponent

--- money-loggering-fe/src/views/Dashboard/components/ActivityTable/subcomponents/TableRow/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-import { TableRowBaseContainer } from "../../styles";
-
-export const Container = styled(TableRowBaseContainer)`
-  border-bottom: 1px solid gray;
-`;
