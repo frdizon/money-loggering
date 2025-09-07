10b1c3b77d55b89b84fd8d0218eec793330612f4
Franz Dizon <frdizon@live.com>
2025-09-07 11:40:46 +0800

    [Infra] Update configs to support DNS

--- money-loggering-fe/vite.config.ts
+++ money-loggering-fe/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,10 +9,10 @@ export default defineConfig({
     },
   },
   server: {
-    port: 3000,
+    port: 80,
     strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:3000",
+    origin: "http://0.0.0.0:80",
   },
   base: "/",
   preview: {
